d03493de0b792caaffe3f03b4c97b6e6bf1f146a
Martin Wiso <martin@wiso.cz>
2017-03-02 16:48:55 +0100

    Add custom exceptions to improve experience

---
+++ talon_one/exceptions.py
@@ -0,0 +1,17 @@
+import requests, cjson
+
+class TalonOneAPIError(Exception):
+    """
+    TalonOneAPIError Exception
+    """
+    def __init__(self, message, *args):
+        self.message = message
+
+        # try to enhance with detailed error from API
+        if len(args) > 0 and isinstance(args[0], requests.exceptions.HTTPError):
+            hints = cjson.decode(args[0].response.text)
+            if "errors" in hints:
+                self.message += " - %s" % hints["errors"][0]["title"]
+            super(TalonOneAPIError, self).__init__(self.message, *args)
+        else:
+            super(TalonOneAPIError, self).__init__(self.message, *args)
