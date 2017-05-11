f5c5c9297bb5c7dfa0ee6c3077329c8e7fa00b06
Martin Wiso <martin@wiso.cz>
2017-05-11 11:02:03 +0200

    Include API validation response into `TalonOneAPIError` details

--- talon_one/exceptions.py
+++ talon_one/exceptions.py
@@ -11,7 +11,11 @@ class TalonOneAPIError(Exception):
         # try to enhance with detailed error from API
         if len(args) > 0 and isinstance(args[0], requests.exceptions.HTTPError):
             hints = json.loads(args[0].response.text)
+
             if "errors" in hints:
                 self.message += " - %s" % hints["errors"][0]["title"]
+
+            if "message" in hints:
+                self.message += " - %s" % hints["message"]
 
         super(TalonOneAPIError, self).__init__(self.message, *args)
